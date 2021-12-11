b6340af976a410420c86cadce6207797a52e9431
lolko <alfavanish@mail.ru>
2021-12-11 22:16:22 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Даны два числа. Показать большее и меньшее число
+void SravnitChisla(int a, int b)
+{
+    if (a > b)
+    {
+        System.Console.WriteLine($"max a = {a}, min b = {b}");
+    }
+    else
+    {
+        System.Console.WriteLine($"max b = {b}, min a = {a}");
+    }
+}
+SravnitChisla(33, 40);

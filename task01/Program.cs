428ed05e0470fce2b003965d859eedafa87dc881
lolko <alfavanish@mail.ru>
2021-12-11 21:35:14 +0300

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+System.Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if(a==b*b)
+{
+    Console.WriteLine("good");
+}
+else
+{
+    Console.WriteLine("hrenovo");
+}
